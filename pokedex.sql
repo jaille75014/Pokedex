fd592e9ccd236aab2ee9786ae1b0fa739c6fdd69
Jules Simon <114732994+jaille75014@users.noreply.github.com>
2024-04-02 18:32:14 +0200

    Add files via upload

--- pokedex.sql
+++ pokedex.sql
@@ -1,48 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 5.1.2
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3306
--- Généré le : mer. 27 mars 2024 à 21:31
--- Version du serveur : 5.7.24
--- Version de PHP : 8.0.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données : `pokedex`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `pokemons`
---
 
 CREATE TABLE `pokemons` (
   `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `pv` int(11) NOT NULL,
-  `attack` int(11) NOT NULL,
-  `defense` int(11) NOT NULL,
-  `speed` int(11) NOT NULL,
-  `image` varchar(128) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `name` varchar(128) DEFAULT NULL,
+  `pv` int(11) DEFAULT NULL,
+  `attack` int(11) DEFAULT NULL,
+  `defense` int(11) DEFAULT NULL,
+  `speed` int(11) DEFAULT NULL,
+  `image` varchar(128) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -52,47 +27,30 @@ CREATE TABLE `users` (
   `image` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Déchargement des données de la table `users`
---
-
 INSERT INTO `users` (`id`, `pseudo`, `email`, `password`, `image`) VALUES
 (1, 'test', 'test@test.com', '8020d1a33b77f2fbcb83d897ffdfbc5f6961a70952501a9d188d4c51fbc5caea', 'default.svg'),
 (2, 'sacha', 'sacha@gmail.com', '5a45b8c84b1913b4fb524660e709d1b81d694a028d2516297b0da4c259acbf7f', 'default.svg'),
 (3, 'mike', 'mike@gmail.com', '2f387d65802ef43966c601a4df111587df9782f8ce8cc8f7e8fa20bfd8a422f9', 'default.svg'),
 (4, 'Snow', 'snow@gmail.com', 'f67a0bec7a089d9f0c49621644be71f87ea24a23d1b66ed6bbb80818fe0f0bcc', 'default.svg');
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `pokemons`
---
 ALTER TABLE `pokemons`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
---
--- Index pour la table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
 
---
--- AUTO_INCREMENT pour la table `pokemons`
---
 ALTER TABLE `pokemons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT pour la table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+
+ALTER TABLE `pokemons`
+  ADD CONSTRAINT `pokemons_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
