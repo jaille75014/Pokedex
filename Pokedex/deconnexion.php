d06f8c7c27978feeb6e601a1c9b7c36686a2f157
jaille75014 <jules91@gmx.com>
2024-04-04 16:44:10 +0200

    deleted:    Pokedex/add_pokemon.php 	deleted:    Pokedex/collection.php 	deleted:    Pokedex/connexion.php 	deleted:    Pokedex/deconnexion.php 	deleted:    Pokedex/index.php 	deleted:    Pokedex/pokedex.sql 	deleted:    Pokedex/profile.php

--- Pokedex/deconnexion.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start(); // Demarrage de la session afin de la détruire
-    session_destroy(); // Destruction de la session
-    header('location:index.php'); // Redirection vers l'accueil
-    exit;
-?>
